0923769b3030c73749544c3d9614de8b9b936576
jack-zdl <zhangdelei@bsgchina.com>
2017-05-11 08:02:37 +0800

    增加arrayList linkedList Vector Stack

--- src/com/zhang/container/TestLinkedList.java
+++ src/com/zhang/container/TestLinkedList.java
@@ -1,36 +1,40 @@
 package com.zhang.container;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 
 /**@author  zhangdelei
  * @data 2017年3月22日下午9:07:17
  *@Description 测试链表（单链表，双链表） 这个是基于双向链表
- *			1二个方法只有返回类型不同是不能区别他的的，
+ *	1   二个方法只有返回类型不同是不能区别他的的，
+ *	2 LinkedList是非线程安全的
  */
 
 public  class TestLinkedList {
 	
-
+	@Test
+	public void testListLinkedList(){
+		List linkedList = new LinkedList();
+		//一样使用list接口的方法
+		linkedList.add("1");
+		System.out.println("链表="+linkedList);
+	}
 	  
 	@Test
 	public void testLink(){
-		 LinkList linkList = new LinkList();  
-	      linkList.addFirstNode(20);  
-	      //linkList.addFirstNode(21);  
-	      //linkList.addFirstNode(19);  
-	       //19,21,20  
-	      linkList.add(1, 22); //19,22,21,20  
-	      linkList.add(2, 23); //19,22,23,21,20  
-	      linkList.add(3, 99); //19,22,23,99,21,20  
-	      linkList.displayAllNodes(); 
-	      Node node = linkList.deleteByData(19);  
-//        Node node = linkList.deleteByPos(0);  
-          System. out.println( "node : " + node. data);  
-          linkList.displayAllNodes();  
-          Node node1 = linkList.findByPos(0);  
-          System. out.println( "node1: " + node1. data);  
-          Node node2 = linkList.findByData(22);  
-          System. out.println( "node2: " + node2. data); 
+		 LinkedList linkedList = new LinkedList(); 
+		 linkedList.addFirst("1");
+		 System.out.println("第一个元素="+linkedList.get(0));
+		// linkedList.remove("1");
+		 System.out.println("linkedList="+linkedList);
+		 Iterator iterator = linkedList.iterator();
+		 while(iterator.hasNext()){
+			 System.out.println("数据="+iterator.next());
+		 }
+		 
 	}
 	
 	
--- src/com/zhang/container/TestStack.java
+++ src/com/zhang/container/TestStack.java
@@ -1,8 +1,27 @@
 package com.zhang.container;
 
+import java.util.Stack;
+
+import org.junit.Test;
+
+/**@author  zhangdelei
+ * @data 2017年5月10日下午9:18:51
+ *@Description stack继承于Vector，实现了后进先出（LIFO）堆栈形式一个桶,提供了push,pop,peek
+ *push 增加一个元素
+ *pop 
+ */
+
 public class TestStack {
-	/**@author  zhangdelei
-	 * @data 2017年5月10日下午9:18:51
-	 *@Description TODO
-	 */
+	
+	@Test
+	public void test(){
+		Stack stack = new Stack();
+		stack.push("1"); //添加元素进入桶
+		System.out.println("stack的堆栈="+stack);
+		stack.pop();//删除最后一个元素
+		System.out.println("stack的堆栈="+stack);
+		stack.peek();//获取最后一个元素
+		System.out.println("stack的堆栈="+stack);
+	}
+	
 }
