f55a800ad712637def6e88593e7ff1001db70c9a
zhangdelei <zhangdelei000@gmail.com>
2018-02-07 20:23:59 +0800

    算法学习

--- src/com/zhang/sort/TestFastSort.java
+++ src/com/zhang/sort/TestFastSort.java
@@ -1,12 +1,77 @@
 package com.zhang.sort;
 
 /**
- * 功能说明: <br>
+ * 功能说明: 快速算法<br>
  * 系统版本: 2.0 <br>
  * 开发人员: zhangdl <br>
  * 开发时间: 2018/2/3 10:52<br>
  */
 
-public
-class TestFastSort {
+public class TestFastSort {
+
+    /**
+     * 查找出中轴（默认是最低位low）的在numbers数组排序后所在位置
+     *
+     * @param numbers 带查找数组
+     * @param low   开始位置
+     * @param high  结束位置
+     * @return  中轴所在位置
+     */
+    public static int getMiddle(int[] numbers, int low,int high)
+    {
+        int temp = numbers[low]; //数组的第一个作为中轴
+        while(low < high)
+        {
+            while(low < high && numbers[high] >= temp)
+            {
+                high--;
+            }
+            numbers[low] = numbers[high];//比中轴小的记录移到低端
+            while(low < high && numbers[low] < temp)
+            {
+                low++;
+            }
+            numbers[high] = numbers[low] ; //比中轴大的记录移到高端
+        }
+        numbers[low] = temp ; //中轴记录到尾
+        return low ; // 返回中轴的位置
+    }
+
+    /**
+     *
+     * @param numbers 带排序数组
+     * @param low  开始位置
+     * @param high 结束位置
+     */
+    public static void quickSort(int[] numbers,int low,int high)
+    {
+        if(low < high)
+        {
+            //将numbers数组进行一分为二
+//    　　  int middle = getMiddle(numbers,low,high);
+            //对低字段表进行递归排序
+//    　　  TestFastSort.quickSort(numbers, low, middle-1);
+            //对高字段表进行递归排序
+//    　　  TestFastSort.quickSort(numbers, middle+1, high);
+        }
+
+    }
+
+    /**
+     * 快速排序
+     * @param numbers 带排序数组
+     */
+    public static void quick(int[] numbers)
+    {
+        if(numbers.length > 0)   //查看数组是否为空
+        {
+            quickSort(numbers, 0, numbers.length-1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] number = {1,5,7,9,2,4,6};
+        TestFastSort.getMiddle(number,0,6);
+    }
 }
