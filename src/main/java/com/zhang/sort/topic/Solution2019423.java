aaaa9c13c95f202141f147d02660d40fb18bab35
zhangdelei <zhangdelei000@gmail.com>
2019-04-25 22:10:22 +0800

    修改代码，抄袭网上的

--- src/main/java/com/zhang/sort/topic/Solution2019423.java
+++ src/main/java/com/zhang/sort/topic/Solution2019423.java
@@ -24,32 +24,51 @@ public class Solution2019423 {
      */
     public  ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        ListNode node = new ListNode(0);
-        if((l1 == null || l1.next == null )&& (l2 == null || l2.next == null)){
-            if(l1 == null){
-                l1 = new ListNode(0);
-            }else if(l2 == null) {
-                l2 = new ListNode(0);
-            }
-            int a = l1.val+l2.val;
-            int  remainder = a % 10 ;
-            int result  = a / 10;
-            node.next = new ListNode(remainder);
-            node.val = result;
-            return node;  // 余数
-        }else {
-            ListNode val = addTwoNumbers(l1.next,l2.next);
-            int b = val.val + l1.val + l2.val;
-           int  remainder = b % 10 ;
-           int result  = b / 10;
-            if(l1.next == null){
-                l1.next = new ListNode(0);
-            }
-            val.val = remainder ;
-            val.next.val = val.next.val +result;
-            node.next = val;
-            return node;
+        ListNode node1 = l1;
+        ListNode node2 = l2;
+        // 寻找长的list
+        while(node1 != null && node2 != null) {
+            node1 = node1.next;
+            node2 = node2.next;
         }
+        // 判断长度
+        if(node2 == null) {
+            node1 = l1;
+            node2 = l2;
+        } else {
+            node1 = l2;
+            node2 = l1;
+        }
+        ListNode result = new ListNode(0);
+        ListNode node = result;
+        // node2 是较短的那个(可能同样长)
+        while(node2 != null) {
+            // 求和
+            int sum = node1.val + node2.val + node.val;
+            node.val = sum % 10;
+            // 进位
+            int mod = sum / 10;
+            if(mod > 0 || node1.next != null) {
+                node.next = new ListNode(mod);
+                node = node.next;
+            }
+            // 后移
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+        while(node1 != null) {
+            // 求和
+            int sum = node1.val + node.val;
+            node.val = sum % 10;
+            // 进位
+            int mod = sum / 10;
+            if(mod > 0 || node1.next != null) {
+                node.next = new ListNode(mod);
+                node = node.next;
+            }
+            node1 = node1.next;
+        }
+        return result;
     }
 
     class ListNode {
@@ -62,18 +81,18 @@ public class Solution2019423 {
     private ListNode l2;
     @Before
     public void before(){
-        listNode = new ListNode(6);
-        listNode.next = new ListNode(6);
-        listNode.next.next = new ListNode(6);
-        l2 = new ListNode(6);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(6);
-        l2.next.next.next =new ListNode(6);
+        listNode = new ListNode(1);
+//        listNode.next = new ListNode(4);
+//        listNode.next.next = new ListNode(3);
+        l2 = new ListNode(9);
+        l2.next = new ListNode(9);
+//        l2.next.next = new ListNode(4);
+//        l2.next.next.next =new ListNode(6);
     }
 
     @Test
     public void Test(){
         ListNode result = addTwoNumbers(listNode,l2);
-        System.out.println("l1 ="+listNode);
+        System.out.println("l1 ="+result);
     }
 }
